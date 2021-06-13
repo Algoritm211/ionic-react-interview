d838c0c8bc57909aaa0caa3c7aa140491e10cea9
alexey_horbunov <algoritm211@gmail.com>
2021-06-13 12:07:55 +0300

    Add separate SpecList.tsx component

---
+++ src/components/SpecList/SpecList.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import Loader from '../Loader/Loader'
+import { IonItem, IonLabel, IonSelect, IonSelectOption, IonTitle, useIonViewDidEnter } from '@ionic/react'
+import { loadAllSpecs } from '../../store/specialists/thunks'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllSpecs, getIsLoading } from '../../store/specialists/selectors'
+import SpecCard from '../specCard/specCard'
+
+type PropsType = {
+  isLiked?: boolean,
+}
+
+const SpecList: React.FC<PropsType> = ({ isLiked }) => {
+  const dispatch = useDispatch()
+  const specs = useSelector(getAllSpecs)
+  const isLoading = useSelector(getIsLoading)
+  const [filter, setFilter] = useState(['all'])
+
+  useIonViewDidEnter(() => {
+    dispatch(loadAllSpecs(filter, isLiked))
+  }, [])
+
+  useEffect(() => {
+    dispatch(loadAllSpecs(filter, isLiked))
+  }, [filter])
+
+  const specsBlock = specs.map((specialist) => {
+    return <SpecCard specialist={specialist} key={specialist.id}/>
+  })
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader/>) : (
+        <>
+          <IonTitle size="large" className={'ion-padding'}>Результаты:</IonTitle>
+          <IonItem className={'ion-margin'}>
+            <IonLabel position="floating">Выберите специалиста</IonLabel>
+            <IonSelect
+              onIonChange={(event) => setFilter([event.detail.value])}
+              value={filter[0]}
+              name={'type'}>
+              <IonSelectOption value={'all'}>Все</IonSelectOption>
+              <IonSelectOption value={'Психолог'}>Психолог</IonSelectOption>
+              <IonSelectOption value={'Психотерапевт'}>Психотерапевт</IonSelectOption>
+              <IonSelectOption value={'Психиатр'}>Психиатр</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+          {specsBlock}
+        </>
+      )}
+    </>
+  )
+}
+
+export default SpecList
--- src/pages/MainListTab/MainListTab.tsx
+++ src/pages/MainListTab/MainListTab.tsx
@@ -1,29 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { IonContent, IonHeader, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react'
-import SpecCard from '../../components/specCard/specCard'
-import { useDispatch, useSelector } from 'react-redux'
-import { getAllSpecs, getIsLoading } from '../../store/specialists/selectors'
-import { loadAllSpecs } from '../../store/specialists/thunks'
-import Loader from '../../components/Loader/Loader'
+import React from 'react'
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
+import SpecList from '../../components/SpecList/SpecList'
 
 const MainListTab: React.FC = () => {
-  const dispatch = useDispatch()
-  const specs = useSelector(getAllSpecs)
-  const isLoading = useSelector(getIsLoading)
-  const [filter, setFilter] = useState(['all'])
-
-  useIonViewDidEnter(() => {
-    dispatch(loadAllSpecs(filter))
-  }, [])
-
-  useEffect(() => {
-    dispatch(loadAllSpecs(filter))
-  }, [filter])
-
-  const specsBlock = specs.map((specialist) => {
-    return <SpecCard specialist={specialist} key={specialist.id}/>
-  })
-
   return (
     <IonPage>
       <IonHeader>
@@ -32,25 +11,7 @@ const MainListTab: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        {isLoading ? (
-          <Loader/>) : (
-          <>
-            <IonTitle size="large" className={'ion-padding'}>Результаты:</IonTitle>
-            <IonItem className={'ion-margin'}>
-              <IonLabel position="floating">Выберите специалиста</IonLabel>
-              <IonSelect
-                onIonChange={(event) => setFilter([event.detail.value])}
-                value={filter[0]}
-                name={'type'}>
-                <IonSelectOption value={'all'}>Все</IonSelectOption>
-                <IonSelectOption value={'Психолог'}>Психолог</IonSelectOption>
-                <IonSelectOption value={'Психотерапевт'}>Психотерапевт</IonSelectOption>
-                <IonSelectOption value={'Психиатр'}>Психиатр</IonSelectOption>
-              </IonSelect>
-            </IonItem>
-            {specsBlock}
-          </>
-        )}
+        <SpecList />
       </IonContent>
     </IonPage>
   )
